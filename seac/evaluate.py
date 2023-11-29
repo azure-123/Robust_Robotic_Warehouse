001647499fc715a5be1bcc703cce00c58e0832c1
Azure <1666534783@qq.com>
2023-11-29 15:50:51 +0800

    add random noise attack

--- seac/evaluate.py
+++ seac/evaluate.py
@@ -7,15 +7,15 @@ from a2c import A2C
 from wrappers import RecordEpisodeStatistics, TimeLimit
 
 # import attack functions
-from attack import fgsm
+from attack import fgsm, rand_noise
 
 path = "/home/gwr/python_projects/Robust_Robotic_Warehouse/seac/results/unzip_models/rware-tiny-4ag-v1/u2000000" #"pretrained/rware-small-4ag"
 env_name = "rware-tiny-4ag-v1"
 time_limit = 500 # 25 for LBF
-adv = "fgsm" # "fgsm", "pgd", "rand_noise", "gaussian_noise"
-epsilon = 0.02 # <=0.02 is the appropriate perturbation size
+adv = None # "fgsm", "pgd", "rand_noise", "gaussian_noise" and None
+epsilon = 0.02 # <=0.02 is the appropriate perturbation size for fgsm
 
-RUN_STEPS = 1500
+RUN_STEPS = 15000
 
 env = gym.make(env_name)
 env = TimeLimit(env, time_limit)
@@ -59,4 +59,17 @@ elif adv == "fgsm":
             print(f"Episode rewards: {sum(info['episode_reward'])}")
             print(info)
             print(" --- ")
-
+elif adv == "rand_noise":
+    for i in range(RUN_STEPS):
+        obs = [torch.from_numpy(o) for o in obs]
+        adv_obs = rand_noise(epsilon, obs)
+        _, actions, _ , _ = zip(*[agent.model.act(adv_obs[agent.agent_id], None, None) for agent in agents])
+        actions = [a.item() for a in actions]
+        # env.render()
+        obs, _, done, info = env.step(actions)
+        if all(done):
+            obs = env.reset()
+            print("--- Episode Finished ---")
+            print(f"Episode rewards: {sum(info['episode_reward'])}")
+            print(info)
+            print(" --- ")
